a6e1aa0cdea08999015a68ccb858d448117cbda2
agrzes <abramczykg@gmail.com>
2016-06-05 21:59:35 +0200

    Weekend service

--- examples/express/route/index.js
+++ examples/express/route/index.js
@@ -1,22 +1,38 @@
 var express = require('express');
 var http = require('http');
+var weekend = require('../../moment/weekend');
 var app = express();
 
 app.set('port', 6666);
 
-app.get('/:first/second/:third',function(request,response,next){
-    response.send("First "+request.params.first+" Second "+request.params.first+" Third "+request.params.first);
+app.get('/:year/:month/:day', function (request, response, next) {
+    try {
+        var nextWeekend = weekend(request.params.year + '-' + request.params.month + '-' + request.params.day);
+        if (nextWeekend.isWeekend()) {
+            response.send('Hooray it is weekend\nNext weekened starting at ' + nextWeekend.nextWeekend().format('L'));
+        } else {
+            response.send('Not weekend\nNext weekened starting at ' + nextWeekend.nextWeekend().format('L') + '\n' + nextWeekend.tillWeekend().humanize() + ' till next weekend');
+        }
+    } catch (e) {
+        console.error(e);
+        response.status(500).send(e);
+    }
 });
 
-app.get('/',function(request,response,next){
-    if (request.query.name){
-        response.send("Hello "+request.query.name+"!");
-    } else {
-        response.send("Hello Express!");
+app.use("/", function (request, response, next) {
+    try {
+        var nextWeekend = weekend(request.query.date);
+        if (nextWeekend.isWeekend()) {
+            response.send('Hooray it is weekend\nNext weekened starting at ' + nextWeekend.nextWeekend().format('L'));
+        } else {
+            response.send('Not weekend\nNext weekened starting at ' + nextWeekend.nextWeekend().format('L') + '\n' + nextWeekend.tillWeekend().humanize() + ' till next weekend');
+        }
+    } catch (e) {
+        console.error(e);
+        response.status(500).send(e);
     }
-
 })
 
-http.createServer(app).listen(app.get('port'), function() {
+http.createServer(app).listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
 });
