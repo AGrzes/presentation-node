a4184ab0c4b00a8e09657039a94c1f8c44313558
agrzes <abramczykg@gmail.com>
2016-06-05 21:52:36 +0200

    Weekend service

--- examples/express/middleware/index.js
+++ examples/express/middleware/index.js
@@ -1,18 +1,24 @@
 var express = require('express');
 var http = require('http');
+var weekend = require('../../moment/weekend');
 var app = express();
 
 app.set('port', 6666);
 
-app.use(function(request,response,next){
-    if (request.query.name){
-        response.send("Hello "+request.query.name+"!");
-    } else {
-        response.send("Hello Express!");
+app.use(function (request, response, next) {
+    try {
+        var nextWeekend = weekend(request.query.date);
+        if (nextWeekend.isWeekend()) {
+            response.send('Hooray it is weekend\nNext weekened starting at ' + nextWeekend.nextWeekend().format('L'));
+        } else {
+            response.send('Not weekend\nNext weekened starting at ' + nextWeekend.nextWeekend().format('L') + '\n' + nextWeekend.tillWeekend().humanize() + ' till next weekend');
+        }
+    } catch (e) {
+        console.error(e);
+        response.status(500).send(e);
     }
-
 })
 
-http.createServer(app).listen(app.get('port'), function() {
+http.createServer(app).listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
 });
